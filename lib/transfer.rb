bea23b744db0a9e0eca88edf45fa17527c1d819f
Marianne Pyon <mariannepyon@gmail.com>
2019-09-03 17:45:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@
  end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.balance >= @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
